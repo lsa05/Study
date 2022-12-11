00b04c4c49994232597526f1c43568d4617d0d52
lsa05 <ssangah05@gmail.com>
2022-12-11 12:51:40 +0900

    스터디

--- Study/src/Algorithm_Study/Ex22.java
+++ Study/src/Algorithm_Study/Ex22.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 
 class Solution7 {
     public int solution(int k, int m, int[] score) { 
-    	//k=최고점, m=개수, p=가장 낮은 점수, 한 상자 가격
+    	//k=최고점, m=개수, p=최저점,			 한 상자 가격=p*m  result=p*m*상자수
         int answer = 0;
-         
-        Arrays.sort(score);			//[4,4,4,4,4,4,2,2,2,2,1,1]          [3,3,2,2,1,1,1]
+        							
+        Arrays.sort(score);			
         
-        int apple = score.length;	// 사과갯수				-> apple=12    =7
-        int extra = apple % m;		// 남은 사과갯수 			-> extra=0     =3       
+        int apple = score.length;	
+        int extra = apple % m;		   
        
-        // [0] [3] [6] [9]                                    [0]
-        //  112 222 444 444   1*3 + 2*3 + 4*3 +4*3 = 33        3322 111
+        
         for (int i=extra; i<apple; i=i+m) {
 		    answer += score[i]*m;
 		}
